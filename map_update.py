61fe4c912f6298782a2ed82a2c1449a040d6444e
Shnavo <szango297@gmail.com>
2026-03-07 15:29:09 +0100

    start of a new project

---
+++ map_update.py
@@ -0,0 +1,13 @@
+import os
+import zipfile
+
+dysk = os.path.getmtime("C:\\Users\\szang\\AppData\\LocalLow\\IronGate\\Valheim\\worlds_local\\Sosnowiec.db")
+with zipfile.ZipFile("C:\\Users\\szang\\Downloads\\drive-download-20260304T111228Z-1-001.zip") as zip:
+    zip.extractall("C:\\Users\\szang\\Downloads\\maps")
+google = os.path.getmtime("C:\\Users\\szang\\Downloads\\maps\\Sosnowiec.db")
+if (dysk > google):
+    print (f"na dysku: {dysk}")
+elif (dysk < google):
+    print(f"na google: {google}")
+else:
+    print("equal")
