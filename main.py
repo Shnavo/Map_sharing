d95b3f3ac95f830f6d0c641f7860f258654bc9db
Shnavo <szango297@gmail.com>
2026-03-09 19:13:56 +0100

    addin google requirements

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import zipfile
 
+from google_auth_oauthlib.flow import Flow
+
 dysk = os.path.getmtime("C:\\Users\\szang\\AppData\\LocalLow\\IronGate\\Valheim\\worlds_local\\Sosnowiec.db")
 with zipfile.ZipFile("C:\\Users\\szang\\Downloads\\drive-download-20260304T111228Z-1-001.zip") as zip:
     zip.extractall("C:\\Users\\szang\\Downloads\\maps")
