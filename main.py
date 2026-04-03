107684a4e381e81d8ebdbeeea49ca1aef10016d8
Shnavo <szango297@gmail.com>
2026-04-03 16:24:55 +0200

    Listing specific files functionality

--- main.py
+++ main.py
@@ -3,5 +3,30 @@ from auth import get_credentials
 from googleapiclient.discovery import build
 
 
+def list_files():
+    creds = get_credentials()
+    service = build("drive", "v3", credentials=creds)
+    file_list = (
+        service.files()
+        .list(
+            q="name contains 'Sosnowiec'",
+            pageSize=10,
+            fields="nextPageToken, files(id, name, modifiedTime, createdTime)",
+        )
+        .execute()
+    )
+
+    items = file_list.get("files", [])
+
+    if not items:
+        print("Nie znaleziono plików.")
+    else:
+        print("Pliki:")
+        for item in items:
+            print(
+                f"{item['name']} ({item['id']}) modified: {item['modifiedTime']} created: {item['createdTime']}"
+            )
+
+
 if __name__ == "__main__":
-    get_credentials()
+    list_files()
