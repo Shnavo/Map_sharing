9036e827543727f7cddd331e0297adcc6b1efb15
Shnavo <szango297@gmail.com>
2026-04-03 14:08:30 +0200

    Clean up of code and file structure

---
+++ auth.py
@@ -0,0 +1,27 @@
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from config import SCOPES
+
+
+def get_credentials():
+    creds = None
+
+    if os.path.exists("credentials\\token.json"):
+        # CHANGE FOR WORKING WITH DIFFERENT DEVICES
+        creds = Credentials.from_authorized_user_file("credentials\\token.json", SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials\\credentials.json", SCOPES
+            )
+            creds = flow.run_local_server()
+
+        with open("credentials\\token.json", "w") as token:
+            token.write(creds.to_json())
+
+    return creds
---
+++ config.py
@@ -0,0 +1,4 @@
+SCOPES = [
+    "https://www.googleapis.com/auth/drive.metadata.readonly",
+    "https://www.googleapis.com/auth/drive.file",
+]
