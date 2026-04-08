63a9625bcad0132e3f444d4fdb351e1fefd0aa10
Shnavo <szango297@gmail.com>
2026-04-08 22:21:21 +0200

    Testing datetime functionality

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from auth import get_credentials
+from credentials.path import PATH
+import datetime as dt
+import os
 
 from googleapiclient.discovery import build
 
 
-def list_files():
+def list_files_drive():
     creds = get_credentials()
     service = build("drive", "v3", credentials=creds)
     file_list = (
@@ -26,7 +29,13 @@ def list_files():
             print(
                 f"{item['name']} ({item['id']}) modified: {item['modifiedTime']} created: {item['createdTime']}"
             )
+            google = dt.datetime.strptime(item["modifiedTime"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            drive = dt.datetime.fromtimestamp(os.path.getctime(PATH))
+            if google > drive:
+                print("google bigger")
+            else:
+                print("drive bigger")
 
 
 if __name__ == "__main__":
-    list_files()
+    list_files_drive()
